8bfe8534533d5ce803aba6360737bf007e27b954
Norman Buruchara <normanburuchara90@gmail.com>
2024-12-09 13:07:06 +0300

    Landing page

--- src/landing/LandingPg.js
+++ src/landing/LandingPg.js
@@ -4,6 +4,19 @@ import styled from 'styled-components'
 
 const Styles = styled.div  `
 
+    // - - FULL PAGE - - //
+
+.full-page {
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+}
+
+.header {
+    height: 10%;
+    background-color: #4497f1;
+}
+
 `
 
 export default class LandingPage extends Component {
@@ -17,7 +30,11 @@ export default class LandingPage extends Component {
     render () {
         return (
             <Styles>
-                
+                <div className='full-page'>
+                    <div className='header'>
+                        
+                    </div>
+                </div>
             </Styles>
         )
     }
