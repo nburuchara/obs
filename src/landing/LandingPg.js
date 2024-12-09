92f57d6719a498ffe2e8372aa140db9cb6fdb6dd
Norman Buruchara <normanburuchara90@gmail.com>
2024-12-09 12:00:35 +0300

    Initial push

---
+++ src/landing/LandingPg.js
@@ -0,0 +1,24 @@
+
+import React, {Component} from 'react'
+import styled from 'styled-components'
+
+const Styles = styled.div  `
+
+`
+
+export default class LandingPage extends Component {
+    constructor () {
+        super()
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return (
+            <Styles>
+                
+            </Styles>
+        )
+    }
+}
