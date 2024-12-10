4ae723725f5e01e15ed9bdc69263f5a0b5d31f88
Norman Buruchara <normanburuchara90@gmail.com>
2024-12-10 04:41:44 +0300

    Landing page

--- src/landing/LandingPg.js
+++ src/landing/LandingPg.js
@@ -54,13 +54,13 @@ const Styles = styled.div  `
     // - - MEETINGS CAROUSEL - - //
 
 .meetings-carousel {
-    width: 50%;
+    width: 65%;
     border: 1px solid black;
     overflow-y: scroll;
 }
 
 .meetings-options {
-    width: 50%;
+    width: 35%;
     border: 1px solid black;
 }
 
@@ -70,11 +70,22 @@ const Styles = styled.div  `
     border-radius: 6px;
     margin: auto;
     margin-top: 5%;
+    padding-left: 2.5%;
+    padding-right: 2.5%;
+}
+
+.carousel-container h2 {
+
 }
 
 .carousel-container label {
-    font-size: 80%;
+    font-size: 77.5%;
     font-weight: normal;
+    padding-left: 2.5%;
+}
+
+.carousel-container h2 {
+
 }
 
 `
@@ -102,7 +113,7 @@ export default class LandingPage extends Component {
                             <div className='meetings-carousel'>
                                 <div className='carousel-container'>
                                     <h2>Tuesday Biblica Meeting<label>Tuesday 6:00 pm</label></h2>
-                                    
+                                    <p>An online Bible study group is a welcoming and interactive community where people gather virtually to explore scripture, share insights, and deepen their faith. Participants from diverse backgrounds connect through video calls, chat platforms, or dedicated apps, fostering spiritual growth and meaningful discussions. Each session often includes reading passages, group reflections, and guided teachings led by a facilitator or pastor. Members support one another through prayer and encouragement, creating a sense of fellowship despite physical distance. Accessible from anywhere, these groups offer flexibility and convenience, making it easy for believers to engage with God’s Word in a supportive online environment.</p>
                                 </div>
                             </div>
                             <div className='meetings-options'>
