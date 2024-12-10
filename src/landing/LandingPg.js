48e98225bb31299122a72e79fc646d29cb7fe4c3
Norman Buruchara <normanburuchara90@gmail.com>
2024-12-10 04:45:33 +0300

    Landing page

--- src/landing/LandingPg.js
+++ src/landing/LandingPg.js
@@ -84,9 +84,10 @@ const Styles = styled.div  `
     padding-left: 2.5%;
 }
 
-.carousel-container h3 {
+.carousel-container p {
     line-height: 1.5;
     font-weight: normal;
+    font-size: 101%;
 }
 
 `
@@ -114,7 +115,7 @@ export default class LandingPage extends Component {
                             <div className='meetings-carousel'>
                                 <div className='carousel-container'>
                                     <h2>Tuesday Biblica Meeting<label>Tuesday 6:00 pm</label></h2>
-                                    <h3>An online Bible study group is a welcoming and interactive community where people gather virtually to explore scripture, share insights, and deepen their faith. Participants from diverse backgrounds connect through video calls, chat platforms, or dedicated apps, fostering spiritual growth and meaningful discussions. Each session often includes reading passages, group reflections, and guided teachings led by a facilitator or pastor. Members support one another through prayer and encouragement, creating a sense of fellowship despite physical distance. Accessible from anywhere, these groups offer flexibility and convenience, making it easy for believers to engage with God’s Word in a supportive online environment.</h3>
+                                    <p>An online Bible study group is a welcoming and interactive community where people gather virtually to explore scripture, share insights, and deepen their faith. Participants from diverse backgrounds connect through video calls, chat platforms, or dedicated apps, fostering spiritual growth and meaningful discussions. Each session often includes reading passages, group reflections, and guided teachings led by a facilitator or pastor. Members support one another through prayer and encouragement, creating a sense of fellowship despite physical distance. Accessible from anywhere, these groups offer flexibility and convenience, making it easy for believers to engage with God’s Word in a supportive online environment.</p>
                                 </div>
                             </div>
                             <div className='meetings-options'>
