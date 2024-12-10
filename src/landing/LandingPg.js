7eec53ba91abbb84e3a2aa2cad2f8d4fb496b268
Norman Buruchara <normanburuchara90@gmail.com>
2024-12-10 15:56:57 +0300

    Landing page

--- src/landing/LandingPg.js
+++ src/landing/LandingPg.js
@@ -84,6 +84,16 @@ const Styles = styled.div  `
     padding-left: 2.5%;
 }
 
+.carousel-container-call-btns button {
+    padding: 1.5%;
+    background-color: #4497f1;
+    width: 15%;
+    font-size: 100%;
+    color: white;
+    border: 1px solid  #4497f1;
+    border-radius: 5px;
+}
+
 .carousel-container p {
     line-height: 1.5;
     font-weight: normal;
@@ -115,6 +125,9 @@ export default class LandingPage extends Component {
                             <div className='meetings-carousel'>
                                 <div className='carousel-container'>
                                     <h2>Tuesday Biblica Meeting<label>Tuesday 6:00 pm</label></h2>
+                                    <div className='carousel-container-call-btns'>
+                                        <button>Zoom</button>
+                                    </div>
                                     <p>An online Bible study group is a welcoming and interactive community where people gather virtually to explore scripture, share insights, and deepen their faith. Participants from diverse backgrounds connect through video calls, chat platforms, or dedicated apps, fostering spiritual growth and meaningful discussions. Each session often includes reading passages, group reflections, and guided teachings led by a facilitator or pastor. Members support one another through prayer and encouragement, creating a sense of fellowship despite physical distance. Accessible from anywhere, these groups offer flexibility and convenience, making it easy for believers to engage with God’s Word in a supportive online environment.</p>
                                 </div>
                             </div>
