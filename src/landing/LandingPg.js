6b6acc9386ef2485e7a3f3b8a313861e95d52c70
Norman Buruchara <normanburuchara90@gmail.com>
2024-12-13 08:52:08 +0300

    Landing page

--- src/landing/LandingPg.js
+++ src/landing/LandingPg.js
@@ -317,6 +317,53 @@ const Styles = styled.div  `
 
 `
 
+//* - TRIE NODE IMPLEMENTATION (for search functionality) - *//
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isEndOfWord = false;
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let current = this.root;
+        for (let char of word) {
+            if (!current.children[char]) {
+                current.children[char] = new TrieNode();
+            }
+            current = current.children[char];
+        }
+        current.isEndOfWord = true;
+    }
+
+    search(word) {
+        let current = this.root;
+        for (let char of word) {
+            if (!current.children[char]) {
+                return false;
+            }
+            current = current.children[char];
+        }
+        return current.isEndOfWord;
+    }
+
+    startsWith(prefix) {
+        let current = this.root;
+        for (let char of prefix) {
+            if (!current.children[char]) {
+                return false;
+            }
+            current = current.children[char];
+        }
+        return true;
+    }
+}
+
 export default class LandingPage extends Component {
     constructor () {
         super()
