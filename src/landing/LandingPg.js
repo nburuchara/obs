7a49abe8d1e1db45c87d9b6f7ca20f37b38d6ca5
Norman Buruchara <normanburuchara90@gmail.com>
2025-06-06 20:25:29 +0300

    Landing page

--- src/landing/LandingPg.js
+++ src/landing/LandingPg.js
@@ -699,8 +699,8 @@ export default class LandingPage extends Component {
         this.state = {
 
             //* - MAIN BODY COMPONENTS - *//
-            showMeetingList: true,
-            showAddMeeting: false,
+            showMeetingList: false,
+            showAddMeeting: true,
 
             //* - SINGLE MEETING COMPONENTS - *//
             isExpanded: false,
